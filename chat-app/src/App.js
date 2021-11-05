257aefd7b71b49a42c4508efc4a7d15bd389d537
Alexander Agyeman <alexagyeman98@gmail.com>
2021-11-05 03:47:43 +0000

    Initial commit

---
+++ chat-app/src/App.js
@@ -0,0 +1,27 @@
+
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '101f0d70-8b44-425a-bd5f-4a9eaf03e414';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
